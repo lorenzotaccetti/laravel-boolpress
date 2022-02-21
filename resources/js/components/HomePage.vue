0e9980cd5d0c5053823291ced9960f214123d0e6
lorenzotaccetti <lorenzo.taccetti2001@gmail.com>
2022-02-21 16:24:33 +0100

    first commit

---
+++ resources/js/components/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="homepage">
+        Benvenuto, questa sarà la homepage del sito!
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomePage'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .homepage{
+        width: 80%;
+        margin: 40px auto;
+        font-size: 20px;
+    }
+</style>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Homepage />
+    </div>
+</template>
+
+<script>
+import Homepage from '../components/HomePage.vue'
+    export default {
+        name: 'App',
+        components: {
+            Homepage
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
