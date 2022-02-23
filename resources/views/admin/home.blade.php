f6e0421476c7ab2fe9ea84b682b69d5ca17cf944
lorenzotaccetti <lorenzo.taccetti2001@gmail.com>
2022-02-23 19:21:09 +0100

    Create categories table and seeder

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,10 +4,12 @@
     <h1>
         Benvenuto {{$user->name}}
     </h1>
-    <div>
-        Cellulare: {{$user_info->phone_number}}
-    </div>
-    <div>
-        Indirizzo: {{$user_info->address}}
-    </div>
+    @if ($user_info)
+        <div>
+            Cellulare: {{$user_info->phone_number}}
+        </div>
+        <div>
+            Indirizzo: {{$user_info->address}}
+        </div>
+    @endif
 @endsection
